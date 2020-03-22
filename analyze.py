244f20acb44b338451314c2ad4ef38e69651999c
Kirill <zarj09@gmail.com>
2020-03-22 17:48:10 +0200

    new

--- analyze.py
+++ analyze.py
@@ -1,7 +1,18 @@
-a=open('C://tmp1/test.txt','r')
+a=open('H://test.txt','r')
 b=a.read()
 s=b.split('\n')
 
+def digit(s):                         
+    s2=''                               #переменая для чисел
+    for i in s:                     #перебор в переданом ф-и списке
+        if i.isdigit()!=True:       #если цифра
+            continue                 #добавляю в перем.s2
+        else:                       #иначе
+            s2+=i                    #пропускаю
+    if len(s2)==0:              #если в списке 0 элементов
+        return 0                #возвращаю 0
+    else:                       #иначе
+        return s2
 
 def analyze(str) :
     symbols = {
@@ -9,7 +20,8 @@ def analyze(str) :
         '-' : 0,
         'alpha' : 0,
         '|' : 0,
-        'digit':0
+        'digit':0,
+		'space':0
     }
     for i in str :
         if i == '=' :
@@ -22,58 +34,66 @@ def analyze(str) :
             symbols['digit']+=1
         if i.isalpha():
             symbols['alpha']+=1
+		#if i == ' ' :
+		#	res['space']+=1
 
     return symbols
 
+def alpha(s):
+    s2=''
+    for i in s:
+        if i.isalpha():
+            s2 += i
+    return s2
 
-state = 0
 
 dbs=[]
-
+dict={}
 for i in s:
     res = analyze(i)
-    if res['=']>10 and state==0:
+    if res['=']>10:
 
-        table=i.strip().strip('=').strip()
-        dbs.append({ "table-name": table, "data" : []})
-        #print(table)
-        state=1
-
-    if res['-'] > 8 and state == 1 :
-        state = 2
-
-    if res['|'] > 2 and res['alpha'] > 0 and state == 2 :
-        name=i.strip().split(' | ')
-        #print(name)
-        fldsName = []
-        for fname in name:
-            fldsName.append(fname)
-            #print(fldsName)
-        state=3
-
-    if res['|'] > 0 and res['-'] > 2 and state == 3 :
-        state = 4
-
-    if res['digit'] > 2 and res['alpha'] >= 0   and state==4:
-        val = i.strip().split('|')
-
-        fldsData = []
-
-        for fname in val:
-            fldsData.append(fname)
-        #print(fldsData)
-        dataItem = {}
-        for j in range(len(fldsName)) :
-            dataItem[fldsName[j]] = fldsData[j]
-        for db in dbs :
-            if db['table-name'] == table :
-                break
-        db['data'].append(dataItem)
-        #state = 5
-
-    if res['-'] > 2 and res['|'] == 0 and state == 4:
-        state = 0
+        table=alpha(i)
 
 
-for i in dbs:
-    print(i)
+		
+
+
+    if res['alpha']>10 and res['digit']<1:
+        name=i.split(' | ')
+
+    if res['|'] >= 3 and res['-'] < 7 and res['digit'] > 1:
+        val = i.split('|')
+
+
+        dict = {}
+        dict['data'] = []
+        t={}
+        for j in range(len(name)):
+            t[name[j]] = val[j]
+
+
+        dict['table_name'] = table
+        dict['data'].append(t)
+
+        dbs.append(dict)
+
+
+n=''
+while n!=0:
+    print('\n\nчто ищем?, sales = нажать 1,customers = нажать 2,orders = нажать 3, 0 = выход: \n\n\n')
+    n=int(input('введите ваш выбор:'))
+
+    if n==1:
+            for i in dbs:
+                if i['table_name']=='sales':
+                    print(i)
+    if n==2:
+            for i in dbs:
+                if i['table_name']=='customers':
+                    print(i)
+    if n==3:
+            for i in dbs:
+                if i['table_name']=='orders':
+                    print(i)
+                    
